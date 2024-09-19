909a85df527c247364a2d723bac462dad11f47f6
Han-28 <hanmerhector@gmail.com>
2024-09-18 19:49:05 -0500

    Formulario...

--- index.php
+++ index.php
@@ -1,15 +1,22 @@
 <?php
-session_star();
+session_start();
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index --</title>
+    <title>-Index-</title>
 </head>
 <body>
-    <h1>Trabajo Grupal</h1>
+    <h2>---Trabajo Grupal---</h2>
+    <form id="formulario-tarea">
+        <label for="tarea">Nombre de la tarea:</label>
+        <input type="text" id="tarea" name="tarea" required>
+       <button type="submit"> Agregar Tarea</button><br><br>
+       <button> <a href="agregar_tarea.php"> Ver lista</a> </button>
+    </form>
+
+    
 </body>
 </html>
